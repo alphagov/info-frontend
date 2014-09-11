a07a0a272e1dde518b39e5b66880dc6b9f3a759f
Jake Benilov <benilov@gmail.com>
2014-09-11 22:16:46 +0000

    Render needs on the info page

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,4 +1,8 @@
+require 'govuk/client/metadata_api'
+
 class InfoController < ApplicationController
   def show
+    metadata = GOVUK::Client::MetadataAPI.new.info(params[:slug])
+    @needs = metadata.fetch("needs")
   end
 end
