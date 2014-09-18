770c88766e4aa0b4223d1810d6afb77a3a9527db
Jake Benilov <benilov@gmail.com>
2014-09-18 18:31:56 +0000

    Refactor LeadMetrics spec

--- spec/models/performance_data/lead_metrics_spec.rb
+++ spec/models/performance_data/lead_metrics_spec.rb
@@ -5,19 +5,21 @@ module PerformanceData
   describe LeadMetrics do
     subject { LeadMetrics.new(data) }
 
-    context "some unique pageviews" do
-      let(:data) { { unique_pageviews: [3, 2, 1] } }
-      its(:unique_pageviews_average) { should eq(2) }
-    end
+    context "unique pageviews" do
+      context "some unique pageviews" do
+        let(:data) { { unique_pageviews: [3, 2, 1] } }
+        its(:unique_pageviews_average) { should eq(2) }
+      end
 
-    context "no unique pageview data" do
-      let(:data) { { unique_pageviews: [] } }
-      its(:unique_pageviews_average) { should eq(0) }
-    end
+      context "no unique pageview data" do
+        let(:data) { { unique_pageviews: [] } }
+        its(:unique_pageviews_average) { should eq(0) }
+      end
 
-    context "average unique pageviews less than 1" do
-      let(:data) { { unique_pageviews: [1, 0] } }
-      its(:unique_pageviews_average) { should eq(0.5) }
+      context "average unique pageviews less than 1" do
+        let(:data) { { unique_pageviews: [1, 0] } }
+        its(:unique_pageviews_average) { should eq(0.5) }
+      end
     end
   end
 end
