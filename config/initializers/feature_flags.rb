711e5c01b3bd917d84046d972d27ab1386d05676
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-09-29 14:51:49 +0100

    Move `FeatureFlags` module closer to `InfoFrontend` declaration
    This deals with an ordering issue and means we can keep our inialisers
    simpler so that they just define the values they need to set.

--- config/initializers/feature_flags.rb
+++
@@ -1,8 +0,0 @@
-module InfoFrontend
-  module FeatureFlags
-    mattr_accessor :show_needs
-  end
-end
-
-# Show user needs by default.
-InfoFrontend::FeatureFlags.show_needs = true
---
+++ config/initializers/show_needs.rb
@@ -0,0 +1,2 @@
+# Show user needs by default. This file might be overriden at deploy.
+InfoFrontend::FeatureFlags.show_needs = true
