3fb74ea4e4b2d6dbd322da649f9a88b692fbb5c8
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-01-10 16:05:30 +0000

    Remove use of GdsApi::Helpers

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -3,14 +3,13 @@ require 'performance_data/statistics'
 require 'performance_data/metrics'
 
 class InfoController < ApplicationController
-  include GdsApi::Helpers
   rescue_from GdsApi::ContentStore::ItemNotFound, with: :not_found
   rescue_from GdsApi::HTTPGone, with: :not_found
   before_action :set_expiry, only: :show
 
   def show
     @slug = parse_slug
-    @content = content_store.content_item(@slug).to_h
+    @content = GdsApi.content_store.content_item(@slug).to_h
     @needs = @content["links"]["meets_user_needs"]
     @statistics = PerformanceData::Statistics.new(@content, @slug)
   end
