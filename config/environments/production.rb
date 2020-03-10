dbe324b9349fa5df4b17aa9c4ec059dcdaa0a9c3
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2020-03-10 13:39:37 +0000

    Remove ActiveRecord setup in production config
    During the rails migration this line was added to the production config:
    `ActiveRecord::Middleware::DatabaseSelector::Resolver::Session`, this
    causes the deploy to fail since the application does not use it's own
    database. Here we have commented it out as it was generated in the rails
    6 upgrade.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  # ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
