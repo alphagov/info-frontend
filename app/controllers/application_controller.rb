a9277d400f24d2cf212e0500392af4438117ffc4
Alex J <alexandru.jurubita@digital.cabinet-office.gov.uk>
2021-07-16 16:06:24 +0300

    Swap to `gem_layout`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Slimmer::Template
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -9,6 +10,8 @@ class ApplicationController < ActionController::Base
       password: ENV.fetch("BASIC_AUTH_PASSWORD"),
     )
   end
+
+  slimmer_template :gem_layout
 
 private
 
